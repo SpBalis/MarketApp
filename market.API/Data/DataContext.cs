edf29ddc9899c62f4dba622e3120185eb0784751
SpBalis <balis.spyridon@gmail.com>
2019-07-07 18:25:39 +0300

    photoUser

--- market.API/Data/DataContext.cs
+++ market.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace market.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
         public DbSet<Value> Values {get;set;}
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photo { get; set; }
     }
 }
